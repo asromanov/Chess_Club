cd5bec8c2c1310498ffec23bae887a1da7c48bf9
miarexy <shkanovairina@gmail.com>
2022-12-05 11:13:08 +0300

    merge

--- client/src/components/OnlinePlayersPage/OnlinePlayersPage.jsx
+++ client/src/components/OnlinePlayersPage/OnlinePlayersPage.jsx
@@ -6,6 +6,7 @@ import {
 import ImageIcon from '@mui/icons-material/Image';
 import { setFriendsListAsync } from '../../redux/actions/friendsActions';
 import BadgeAvatar from './BadgeAvatar';
+import { setAcceptInvite, setSendInvite } from '../../redux/actions/gameActions';
 
 export default function OnlinePlayersPage() {
   const { friendsList = [], friendsOnline = [] } = useSelector((state) => state.friends);
@@ -16,37 +17,25 @@ export default function OnlinePlayersPage() {
   }, []);
 
   return (
-    <>
-      <List style={{ margin: '50px', backgroundColor: 'inherit' }} sx={{ width: '100%' }}>
-        {friendsList?.map((friend) => {
-          const isOnline = friendsOnline?.map((el) => el.id).includes(friend.id);
-          return (
-            <ListItem key={friend?.id}>
-              <ListItemAvatar>
-                <BadgeAvatar alt={`${friend.name}`} src="" isOnline={isOnline}>
-                  <ImageIcon />
-                </BadgeAvatar>
-              </ListItemAvatar>
-              <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1rem' }}>
-                <ListItemText primary={`${friend?.name}`} />
-                <Button>Пригласить</Button>
-                <Button color="success">Принять</Button>
-                <Button color="error">Отклонить</Button>
-              </div>
-            </ListItem>
-          );
-        })}
-      </List>
-
-      {/* {users.includes(user.id)
-      {users.includes(user?.id)
-        ? <Button>Пригласить</Button>
-        : (
-          <>
-            <Button>Принять</Button>
-            <Button>Отклонить</Button>
-          </>
-        )} */}
-    </>
+    <List style={{ margin: '50px', backgroundColor: 'inherit' }} sx={{ width: '100%' }}>
+      {friendsList?.map((friend) => {
+        const isOnline = friendsOnline?.map((el) => el.id).includes(friend.id);
+        return (
+          <ListItem key={friend?.id}>
+            <ListItemAvatar>
+              <BadgeAvatar alt={`${friend.name}`} src="" isOnline={isOnline}>
+                <ImageIcon />
+              </BadgeAvatar>
+            </ListItemAvatar>
+            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1rem' }}>
+              <ListItemText primary={`${friend?.name}`} />
+              <Button onClick={() => dispatch(setSendInvite())}>Пригласить</Button>
+              <Button color="success" onClick={() => dispatch(setAcceptInvite())}>Принять</Button>
+              <Button color="error" onClick={() => dispatch()}>Отклонить</Button>
+            </div>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 }
